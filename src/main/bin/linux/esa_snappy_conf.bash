f29ffc798f4c3e8b0b7428f15ed81e39d924865d
Olaf Danne <olaf.danne@brockmann-consult.de>
2022-10-16 18:01:18 +0200

    improved esa_snappy configuration scripts for Win and Linux

--- src/main/bin/linux/esa_snappy_conf.bash
+++ src/main/bin/linux/esa_snappy_conf.bash
@@ -3,8 +3,9 @@
 # Runs the configuration of Python 'esa_snappy' module.
 # Requires proper setup of the environment, i.e.
 # - SNAP and Python need to be installed
+# - external plugin 'esa_snappy' must be installed in SNAP
 # - SNAP_HOME and PYTHONHOME need to be set
-# 
+#
 # e.g.
 # SNAP_HOME=/home/olafd/snap_9.0.0
 # PYTHONHOME=PYTHONHOME=/home/olafd/miniconda3
@@ -21,9 +22,8 @@ fi
 
 JPY_JAR=${SNAP_HOME}/snap/modules/ext/org.esa.snap.esa_snappy/org-jpy/jpy.jar
 SNAP_RUNTIME_JAR=${SNAP_HOME}/snap/modules/ext/org.esa.snap.snap-rcp/org-esa-snap/snap-runtime.jar
-ESA_SNAPPY_JAR=${SNAP_HOME}/snap/modules/org-esa-snap-esa_snappy.jar
 
-${SNAP_HOME}/jre/bin/java.exe \
--cp ${SNAP_HOME}/snap/modules/*;${JPY_JAR};${SNAP_RUNTIME_JAR};${ESA_SNAPPY_JAR}; \
-org.esa.snap.main.EsaSnappyConfigurator \
-${PYTHONHOME}/bin/python
+JARS=${SNAP_HOME}/snap/modules/*:${JPY_JAR}:${SNAP_RUNTIME_JAR}
+
+echo "${SNAP_HOME}/jre/bin/java -cp ${JARS} org.esa.snap.main.EsaSnappyConfigurator ${PYTHONHOME}/bin/python"
+${SNAP_HOME}/jre/bin/java -cp ${JARS} org.esa.snap.main.EsaSnappyConfigurator ${PYTHONHOME}/bin/python
--- src/main/bin/win64/esa_snappy_conf.bat
+++ src/main/bin/win64/esa_snappy_conf.bat
@@ -3,6 +3,7 @@
 :: Runs the configuration of Python 'esa_snappy' module.
 :: Requires proper setup of the environment, i.e.
 :: - SNAP and Python need to be installed
+:: - external plugin 'esa_snappy' must be installed in SNAP
 :: - SNAP_HOME and PYTHONHOME need to be set
 
 :: e.g. 
@@ -14,9 +15,7 @@ IF "%PYTHONHOME%"=="" ECHO ERROR: Variable PYTHONHOME is NOT defined. & exit /b 
 
 set JPY_JAR=%SNAP_HOME%\snap\modules\ext\org.esa.snap.esa_snappy\org-jpy\jpy.jar
 set SNAP_RUNTIME_JAR=%SNAP_HOME%\snap\modules\ext\org.esa.snap.snap-rcp\org-esa-snap\snap-runtime.jar
-set ESA_SNAPPY_JAR=%SNAP_HOME%\snap\modules\org-esa-snap-esa_snappy.jar
 
-%SNAP_HOME%\jre\bin\java.exe ^
--cp "%SNAP_HOME%\snap\modules\*;%JPY_JAR%;%SNAP_RUNTIME_JAR%;%ESA_SNAPPY_JAR%;" ^
-org.esa.snap.main.EsaSnappyConfigurator ^
-%PYTHONHOME%\python.exe
+set JARS=%SNAP_HOME%\snap\modules\*;%JPY_JAR%;%SNAP_RUNTIME_JAR%
+
+%SNAP_HOME%\jre\bin\java.exe -cp "%JARS%" org.esa.snap.main.EsaSnappyConfigurator %PYTHONHOME%\python.exe
