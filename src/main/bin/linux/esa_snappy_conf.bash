b4374c4707f205269fd72675a9dc74bb927cbb7f
Olaf Danne <olaf.danne@brockmann-consult.de>
2022-10-17 10:08:08 +0200

    added usage description in esa_snappy_conf.bash

--- src/main/bin/linux/esa_snappy_conf.bash
+++ src/main/bin/linux/esa_snappy_conf.bash
@@ -4,26 +4,29 @@
 # Requires proper setup of the environment, i.e.
 # - SNAP and Python need to be installed
 # - external plugin 'esa_snappy' must be installed in SNAP
-# - SNAP_HOME and PYTHONHOME need to be set
+# - SNAP_HOME needs to be set
 #
 # e.g.
 # SNAP_HOME=/home/olafd/snap_9.0.0
-# PYTHONHOME=PYTHONHOME=/home/olafd/miniconda3
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters."
+    echo "Usage: esa_snappy_conf.bash <python_exec>"
+    echo "    - with <python_exec>: Full path to Python executable to be used with SNAP, e.g. /home/olafd/miniconda/bin/python"
+    exit 1
+fi
 
 if [[ -z "${SNAP_HOME}" ]]; then
   echo "ERROR: Environment variable SNAP_HOME is not set."
   exit 1
 fi
 
-if [[ -z "${PYTHONHOME}" ]]; then
-  echo "ERROR: Environment variable PYTHONHOME is not set."
-  exit 1
-fi
+PYTHON_EXEC=$1
 
 JPY_JAR=${SNAP_HOME}/snap/modules/ext/org.esa.snap.esa_snappy/org-jpy/jpy.jar
 SNAP_RUNTIME_JAR=${SNAP_HOME}/snap/modules/ext/org.esa.snap.snap-rcp/org-esa-snap/snap-runtime.jar
 
 JARS=${SNAP_HOME}/snap/modules/*:${JPY_JAR}:${SNAP_RUNTIME_JAR}
 
-echo "${SNAP_HOME}/jre/bin/java -cp ${JARS} org.esa.snap.main.EsaSnappyConfigurator ${PYTHONHOME}/bin/python"
-${SNAP_HOME}/jre/bin/java -cp ${JARS} org.esa.snap.main.EsaSnappyConfigurator ${PYTHONHOME}/bin/python
+echo "${SNAP_HOME}/jre/bin/java -cp ${JARS} org.esa.snap.main.EsaSnappyConfigurator ${PYTHON_EXEC}"
+${SNAP_HOME}/jre/bin/java -cp ${JARS} org.esa.snap.main.EsaSnappyConfigurator ${PYTHON_EXEC}
