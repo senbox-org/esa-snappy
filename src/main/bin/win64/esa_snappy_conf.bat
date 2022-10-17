abaf5accdb0a01f302fbda2b1770d0a4cc33231f
Olaf Danne <olaf.danne@brockmann-consult.de>
2022-10-17 09:55:08 +0200

    added usage description in esa_snappy_conf.bat

--- src/main/bin/win64/esa_snappy_conf.bat
+++ src/main/bin/win64/esa_snappy_conf.bat
@@ -4,18 +4,32 @@
 :: Requires proper setup of the environment, i.e.
 :: - SNAP and Python need to be installed
 :: - external plugin 'esa_snappy' must be installed in SNAP
-:: - SNAP_HOME and PYTHONHOME need to be set
+:: - SNAP_HOME needs to be set
 
 :: e.g. 
-:: SNAP_HOME=D:\olaf\bc\snap-snapshots\9\9.0-release\snap
-:: PYTHONHOME=D:\olaf\Anaconda3
+:: SNAP_HOME=D:\\snap\9.0\snap
+
+if [%1] == [] goto Usage
+if [%1] == [/?] goto Usage
+if not [%2] == [] goto Usage
 
 IF "%SNAP_HOME%"=="" ECHO ERROR: Variable SNAP_HOME is NOT defined. & exit /b 1
-IF "%PYTHONHOME%"=="" ECHO ERROR: Variable PYTHONHOME is NOT defined. & exit /b 1
 
 set JPY_JAR=%SNAP_HOME%\snap\modules\ext\org.esa.snap.esa_snappy\org-jpy\jpy.jar
 set SNAP_RUNTIME_JAR=%SNAP_HOME%\snap\modules\ext\org.esa.snap.snap-rcp\org-esa-snap\snap-runtime.jar
 
 set JARS=%SNAP_HOME%\snap\modules\*;%JPY_JAR%;%SNAP_RUNTIME_JAR%
 
-%SNAP_HOME%\jre\bin\java.exe -cp "%JARS%" org.esa.snap.main.EsaSnappyConfigurator %PYTHONHOME%\python.exe
+%SNAP_HOME%\jre\bin\java.exe -cp "%JARS%" org.esa.snap.main.EsaSnappyConfigurator %1
+
+goto End
+
+:Usage
+@echo Configures the ESA SNAP-Python interface 'esa_snappy'.
+@echo Usage:
+@echo.
+@echo %~n0 Python
+@echo.
+@echo     Python: Full path to Python executable to be used with SNAP, e.g. C:\Miniconda\python.exe
+@echo.
+:End
