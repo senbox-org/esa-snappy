653e81782d4b6c9ca3dff0ebbb2428f7b8ef1058
TomBlock <tom.block@brockmann-consult.de>
2024-10-01 09:27:45 +0200

    updated paths - Linux and MacOS compatibility

--- src/test/java/eu/esa/snap/snappy/PyBridgeTest.java
+++ src/test/java/eu/esa/snap/snappy/PyBridgeTest.java
@@ -2,8 +2,9 @@ package eu.esa.snap.snappy;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import java.io.File;
+
+import static org.junit.Assert.*;
 
 /**
  * @author Olaf Danne
@@ -14,25 +15,24 @@ public class PyBridgeTest {
     public void testEncodeJarPath() {
         final String prefix = "jar:";
         final String suffix = "!/";
+        final String sep = File.separator;
 
         // test for default Windows SNAP installation dir
         String uriStringInput = "jar:file:///C:/Program Files/esa-snap/snap/modules/eu-esa-snap-esa-snappy.jar!/";
         String encodedJarPath = PyBridge.encodeJarPath(prefix, suffix, uriStringInput);
         assertNotNull(encodedJarPath);
-        assertEquals("C:\\Program Files\\esa-snap\\snap\\modules\\eu-esa-snap-esa-snappy.jar", encodedJarPath);
+        assertTrue(encodedJarPath.contains("C:" + sep + "Program Files" + sep + "esa-snap" + sep + "snap" + sep + "modules" + sep + "eu-esa-snap-esa-snappy.jar"));
 
         uriStringInput = "jar:file:///C:/Program%20Files/esa-snap/snap/modules/eu-esa-snap-esa-snappy.jar!/";
         encodedJarPath = PyBridge.encodeJarPath(prefix, suffix, uriStringInput);
         assertNotNull(encodedJarPath);
-        assertEquals("C:\\Program Files\\esa-snap\\snap\\modules\\eu-esa-snap-esa-snappy.jar", encodedJarPath);
+        assertTrue(encodedJarPath.contains("C:" + sep + "Program Files" + sep + "esa-snap" + sep + "snap" + sep + "modules" + sep + "eu-esa-snap-esa-snappy.jar"));
 
         // insert some weird chars, should still work
-        uriStringInput =
-                "jar:file:///C:/Prog_ram Files/../esa-snap/s{na}p/m^odules/eu-esa-sn`ap-esa-snappy.jar!/";
+        uriStringInput = "jar:file:///C:/Prog_ram Files/../esa-snap/s{na}p/m^odules/eu-esa-sn`ap-esa-snappy.jar!/";
 
         encodedJarPath = PyBridge.encodeJarPath(prefix, suffix, uriStringInput);
         assertNotNull(encodedJarPath);
-        System.out.println("encodedJarPath = " + encodedJarPath);
-        assertEquals("C:\\Prog_ram Files\\..\\esa-snap\\s{na}p\\m^odules\\eu-esa-sn`ap-esa-snappy.jar", encodedJarPath);
+        assertTrue(encodedJarPath.contains("C:" + sep + "Prog_ram Files" + sep + ".." + sep + "esa-snap" + sep + "s{na}p" + sep + "m^odules" + sep + "eu-esa-sn`ap-esa-snappy.jar"));
     }
 }
